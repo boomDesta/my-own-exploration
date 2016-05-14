cdfdf571fd8e2eddf8d728cdf2a0b7e2e4ffd2ea
Desta <destachi96@outlook.com>
2016-05-14 15:48:12 -0700

    Update MessageTransmitter.java

--- MessageTransmitter.java
+++ MessageTransmitter.java
@@ -9,11 +9,11 @@ import java.util.logging.Logger;
  * Created by desta on 2016-04-27.
  */
 public class MessageTransmitter extends Thread{
-    String message, hostname;
-    int port;
+    String message;//what to send 
+    String hostname;//where to send message
+    int port;//what port to send message to
     public MessageTransmitter(){
-
-    }
+    }// Is thie necessary?
     public MessageTransmitter(String message, String hostname, int port){
         this.hostname = hostname;
         this.message = message;
@@ -23,9 +23,9 @@ public class MessageTransmitter extends Thread{
     @Override
     public void run() {
         try {
-            Socket s = new Socket(hostname, port);
-            s.getOutputStream().write(message.getBytes());
-            s.close();
+            Socket s = new Socket(hostname, port);//socket connects to a server socket, serversocket receives a socket.
+            s.getOutputStream().write(message.getBytes());//send the output on the screen
+            s.close();//done
         } catch (IOException e) {
             Logger.getLogger(MessageTransmitter.class.getName()).log(Level.SEVERE,null,e);
         }
